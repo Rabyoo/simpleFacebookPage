6f397d5c149a2c427fec0e09dc9d4986b973f1ea
Reda Maher Aziz <129579969+Rabyoo@users.noreply.github.com>
2023-11-11 11:26:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// let head_icon = document.querySelector(
+//   ".container header .top .head-icons .fa-display"
+// );
+
+// head_icon.onclick = () => {
+//   head_icon.style.borderBottom = "3px solid red";
+// };
